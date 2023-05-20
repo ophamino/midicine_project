19fc99db6b1433d19714e7b8bc027892875a015b
Alisultanoc Dzhamaludin <alisultanovdzh@gmail.com>
2023-05-20 17:22:11 +0300

    add swagger

--- backend/account/serialazers.py
+++ backend/account/serialazers.py
@@ -7,4 +7,8 @@ class UsersSerialazer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'first_name', 'last_name', 'email', 'phone', 'position', 'departament']
-        
+
+class UserProfileChangeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'email', 'phone', 'position', 'departament']
--- backend/account/views.py
+++ backend/account/views.py
@@ -1,11 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 
-from rest_framework import generics
+from rest_framework import generics, permissions, mixins
 
-from .serialazers import UsersSerialazer
+from .permissions import UserIsOwnerOrReadOnly
+from .serialazers import UsersSerialazer, UserProfileChangeSerializer
 from .models import CustomUser
+
+User = get_user_model()
 
 
 class UserAPIListView(generics.ListAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UsersSerialazer
+
+
+class UserProfileChangeAPIView(generics.RetrieveAPIView,
+                               mixins.DestroyModelMixin,
+                               mixins.UpdateModelMixin):
+    permission_classes = (
+        permissions.IsAuthenticated,
+        UserIsOwnerOrReadOnly,
+    )
+    serializer_class = UserProfileChangeSerializer
+
+    def get_object(self):
+        user_id = self.kwargs["id"]
+        obj = get_object_or_404(User, id=user_id)
+        return obj
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
