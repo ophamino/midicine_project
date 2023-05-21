c4b9f60eeb11f8e1ed9ff2058d4c6779bd542d3d
Alisultanoc Dzhamaludin <alisultanovdzh@gmail.com>
2023-05-21 09:18:59 +0300

    add list exam

--- backend/quiz/urls.py
+++ backend/quiz/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import QuestionListAPIView, AnswerListAPIView, ExamListAPIView
+from .views import QuestionListAPIView, AnswerListAPIView, ExamListAPIView, ExamListAPIViewSet
 
 
 urlpatterns = [
-    path('<int:pk>/', ExamListAPIView.as_view({'get': 'list'})),
+    path('<int:pk>/', ExamListAPIViewSet.as_view({'get': 'list'})),
     path('question/<int:pk>/', QuestionListAPIView.as_view({'get': 'list'})),
     path('answer/<int:pk>/', AnswerListAPIView.as_view({'get': 'list'})),
+    path('list/', ExamListAPIView.as_view())
 ]
